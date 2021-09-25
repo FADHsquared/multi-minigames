64bcf5a3e2aa4f3895bed8463f98acef7eb7cbe6
FADHsquared <77851256+FADHsquared@users.noreply.github.com>
2021-09-25 14:45:48 +0700

    tictactoe

--- client/src/pages/Games/TicTacToe.vue
+++ client/src/pages/Games/TicTacToe.vue
@@ -1,5 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const board = ref<('x' | 'o' | null)[][]>([
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+])
+const currentTurnIsO = ref(false)
+
+function handleEntryClick(row: number, entry: number) {
+  board.value[row][entry] = currentTurnIsO.value ? 'o' : 'x'
+  currentTurnIsO.value = !currentTurnIsO.value
+}
+</script>
 
 <template>
   <p>tic tac toe works!</p>
+  <div v-for="row in board">
+    <button
+      v-for="entry in row"
+      class="w-8 h-8 border-2"
+      @click="handleEntryClick(board.indexOf(row), row.indexOf(entry))"
+    >
+      {{ entry }}
+    </button>
+  </div>
 </template>
