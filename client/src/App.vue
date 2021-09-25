9fabd3f587b66c2ba268fcd173420026c7692344
FADHsquared <77851256+FADHsquared@users.noreply.github.com>
2021-09-25 18:27:14 +0700

    Get tic-tac-toe working (diagonal win case not handled yet)

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,14 @@ import TheNav from './components/TheNav.vue'
 </script>
 
 <template>
-  <div class="bg-gray-50 dark:bg-gray-800 dark:text-gray-50 min-h-screen">
+  <div
+    class="
+      min-h-screen
+      flex flex-col
+      bg-gray-50
+      dark:bg-gray-800 dark:text-gray-50
+    "
+  >
     <TheNav />
     <router-view />
   </div>
--- client/src/pages/Games/TicTacToe.vue
+++ client/src/pages/Games/TicTacToe.vue
@@ -1,28 +1,77 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { X, Circle } from 'lucide-vue-next'
 
-const board = ref<('x' | 'o' | null)[][]>([
+type Entry = null | 'x' | 'o'
+type Board = Entry[][]
+
+const board = ref<Board>([
   [null, null, null],
   [null, null, null],
   [null, null, null]
 ])
 const currentTurnIsO = ref(false)
+const winner = ref<Entry>(null)
 
-function handleEntryClick(row: number, entry: number) {
-  board.value[row][entry] = currentTurnIsO.value ? 'o' : 'x'
-  currentTurnIsO.value = !currentTurnIsO.value
+function handleEntryClick(rowIdx: number, entryIdx: number) {
+  if (!board.value[rowIdx][entryIdx] && !winner.value) {
+    board.value[rowIdx][entryIdx] = currentTurnIsO.value ? 'o' : 'x'
+    currentTurnIsO.value = !currentTurnIsO.value
+
+    const rotated90DegsBoard: Board = [[], [], []]
+    board.value.forEach((row, rowIdx) => {
+      row.forEach((entry, entryIdx) => {
+        rotated90DegsBoard[entryIdx][rowIdx] = entry
+      })
+    })
+
+    if (
+      board.value.some((row) => row.every((entry) => entry === 'o')) ||
+      rotated90DegsBoard.some((row) => row.every((entry) => entry === 'o'))
+    )
+      winner.value = 'o'
+    if (
+      board.value.some((row) => row.every((entry) => entry === 'x')) ||
+      rotated90DegsBoard.some((row) => row.every((entry) => entry === 'x'))
+    )
+      winner.value = 'x'
+  }
+}
+function handleResetClick() {
+  board.value = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ]
+  winner.value = null
 }
 </script>
 
 <template>
-  <p>tic tac toe works!</p>
-  <div v-for="row in board">
+  <main class="flex-grow flex flex-col items-center justify-center gap-y-2">
+    <p v-if="winner">{{ winner }} won!</p>
+    <div class="w-max divide-y-2 divide-gray-400">
+      <div
+        v-for="(row, rowIdx) in board"
+        :key="rowIdx"
+        class="divide-x-2 divide-gray-400"
+      >
+        <button
+          v-for="(entry, entryIdx) in row"
+          :key="entryIdx"
+          class="align-bottom w-16 h-16 inline-flex items-center justify-center"
+          @click="handleEntryClick(rowIdx, entryIdx)"
+        >
+          <Circle v-if="entry === 'o'" />
+          <X v-if="entry === 'x'" />
+        </button>
+      </div>
+    </div>
     <button
-      v-for="entry in row"
-      class="w-8 h-8 border-2"
-      @click="handleEntryClick(board.indexOf(row), row.indexOf(entry))"
+      class="px-1 border-2 border-gray-400 active:bg-blue-600/25"
+      @click="handleResetClick"
     >
-      {{ entry }}
+      Reset Board
     </button>
-  </div>
+  </main>
 </template>
