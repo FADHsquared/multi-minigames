fb810f331ce912f25e9e6aa6ab6fe541f84c5a5e
FADHsquared <77851256+FADHsquared@users.noreply.github.com>
2021-09-26 00:53:19 +0700

    Refactor eslintrc.js

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,12 +1,12 @@
-{
-  "env": {
-    "browser": true,
-    "node": true
+module.exports = {
+  env: {
+    browser: true,
+    node: true
   },
-  "extends": ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
-  "parser": "vue-eslint-parser",
-  "parserOptions": {
-    "parser": "@typescript-eslint/parser",
-    "sourceType": "module"
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
   }
 }
