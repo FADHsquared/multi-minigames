83151baba3cb0559e286e97712d1d1a5c727c91d
FADHsquared <77851256+FADHsquared@users.noreply.github.com>
2021-10-05 00:20:14 +0700

    Install Daisy UI, uninstall @tailwindcss/forms

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,10 +3,10 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {}
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require('daisyui')]
 }
