352156ef5386a738256283ba8a044913d8f3694e
FADHsquared <77851256+FADHsquared@users.noreply.github.com>
2021-10-05 23:14:14 +0700

    Add Sequelize and Sequelize CLI to server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common'
+import { SequelizeModule } from '@nestjs/sequelize'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { EventsModule } from './events/events.module'
 
 @Module({
-  imports: [EventsModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mariadb',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'multi_minigames',
+      models: []
+    }),
+    EventsModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
