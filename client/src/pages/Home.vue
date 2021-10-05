035f4392c3efa4ccc00028d8b7817b7804a88f0d
FADHsquared <77851256+FADHsquared@users.noreply.github.com>
2021-10-05 18:59:00 +0700

    Add "featured games" image (preparation for carousel)

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -17,7 +17,7 @@ const contributionMethods = [
     name: 'JavaScript code',
     icon: Code2,
     description:
-      'Submit a PR or contact us on Discord. We recommend sending us a framework-agnostic module for your game.'
+      'Submit a PR or contact our Discord. We suggest you send a framework-agnostic module for your game.'
   },
   {
     name: 'Vue code',
@@ -29,26 +29,57 @@ const contributionMethods = [
 </script>
 
 <template>
-  <h1 class="my-5 text-center text-5xl font-semibold">Multi Minigames</h1>
+  <h1 class="mt-6 mb-3 text-center text-5xl font-semibold">Multi Minigames</h1>
   <p class="text-center">
     A collection of small games, playable offline or online.
   </p>
-  <h2 class="my-2.5 text-center text-3xl font-semibold">Contribute</h2>
+  <h2 class="my-3 text-center text-3xl font-semibold">
+    Featured External Games
+  </h2>
+  <div class="self-center w-[calc(100%-3rem)] max-w-4xl relative">
+    <img
+      src="../assets/koddl-io-screenshot.png"
+      alt="koddl.io"
+      class="rounded-box"
+    />
+    <div
+      class="
+        card
+        bg-secondary
+        text-accent-content
+        shadow
+        absolute
+        bottom-3
+        left-1/2
+        -translate-x-1/2
+      "
+    >
+      <div class="card-body">
+        <div class="card-title">koddl.io</div>
+        <p>The skribbl for coding students.</p>
+        <a href="https://koddl-io.herokuapp.com/" class="link"
+          >https://koddl-io.herokuapp.com/</a
+        >
+      </div>
+    </div>
+  </div>
+  <h2 class="my-3 text-center text-3xl font-semibold">Contribute</h2>
   <div
     class="
       flex flex-col
-      sm:flex-row
-      gap-y-4 gap-x-2
+      md:flex-row
+      gap-y-3 gap-x-3
       max-w-xs
-      sm:max-w-5xl
+      md:max-w-5xl
       mx-auto
-      px-2
+      px-3
+      mb-3
     "
   >
     <div
       v-for="contributionMethod in contributionMethods"
       :key="contributionMethod.name"
-      class="card bordered"
+      class="card bordered flex-1"
     >
       <div class="card-body flex flex-col items-center">
         <component :is="contributionMethod.icon" />
